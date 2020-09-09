0f3c7259fb9fdbd4bc69e35a6dc9d6444f48ecaa
Raldylopez20 <raldysteven"hotmail.com>
2020-09-08 23:19:14 -0400

    Carpeta inicial

---
+++ Cap.#1/Ejercicio1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Raldy Lopez!");
+            
+        }
+    }
+}
---
+++ Cap.#1/Ejercicio5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ejercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Agregue mas mensajes a la aplicacion que ha creado");
+            Console.WriteLine("Hola ");
+            Console.WriteLine("Mi nombre es:"); 
+            Console.WriteLine("Raldy Lopez");
+         }
+    }
+}
